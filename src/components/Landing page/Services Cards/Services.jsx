958f4eb4aab22e96195ad33ed6dc536e483e323f
omaima naeem <omaimanaeem322@gmail.com>
2024-03-03 20:37:52 +0500

    services-cards

--- src/components/Landing page/Services Cards/Services.jsx
+++ src/components/Landing page/Services Cards/Services.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import './services.css';
+import { servicesData1 } from './constant';
+import { servicesData2} from './constant';
 
-const Services = () => {
-  return (
-    <div>
-      <h1>Services card</h1>
-    </div>
-  )
-}
+const Services = () => {return( 
+ <div className="services-container1">
+  <h3 className='heading'>OUR SERVICES</h3>
+  <div className='right'>
+        {servicesData1.map((service) => (
+      <div key={service.id} className="service-card1">
+        <img src={service.icon} alt={service.title} className="service-icon" />
+        <h3 className="service-title">{service.title}</h3>
+        <p className="service-description">{service.description}</p>
+        </div>
+    ))}
+</div>
+<div className='left'>
+      {servicesData2.map((service) => (
+        <div key={service.id} className="service-card2">
+        <img src={service.icon} alt={service.title} className="service-icon" />
+        <h3 className="service-title">{service.title}</h3>
+        <p className="service-description">{service.description}</p>
+        </div>
+         ))}
+</div>  
+</div>
+  )}
+
+
 
 export default Services
